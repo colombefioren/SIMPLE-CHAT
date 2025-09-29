bf21c69f9273238ea1bf0628dcd9306c020747d5
colombefioren <colomberakotonjanahary@gmail.com>
2025-09-29 15:47:14 +0300

    feat: replace mock data with actual dynamic values

--- src/components/chat/chat-message.tsx
+++ src/components/chat/chat-message.tsx
@@ -1,29 +1,33 @@
-const ChatMessage = () => {
-    const mockMessages = [
-        { sender: "you", message: "Hello, how are you?" },
-        { sender: "Jane", message: "I'm doing well, thanks for asking." },
-    ]
+import { Message } from "@/types/chat";
+
+const ChatMessage = ({
+  messageList,
+  userId,
+}: {
+  messageList: Message[];
+  userId: string;
+}) => {
   return (
     <div className="flex flex-col space-y-6">
-      {mockMessages.map((message, index) => (
+      {messageList.map((message, index) => (
         <div
           key={index}
           className={`flex items-center ${
-            message.sender === "you" ? "justify-end" : "justify-start"
+            message.senderId === userId ? "justify-end" : "justify-start"
           }`}
         >
           <div
             className={`px-4 py-2 rounded-lg ${
-              message.sender === "you"
+              message.senderId === userId
                 ? "bg-indigo-500 text-white"
                 : "bg-gray-200 text-gray-800"
             }`}
           >
-            {message.message}
+            {message.content}
           </div>
         </div>
       ))}
     </div>
-  )
-}
-export default ChatMessage
+  );
+};
+export default ChatMessage;
--- src/components/chat/chat-window.tsx
+++ src/components/chat/chat-window.tsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
 import ChatMessage from "./chat-message";
-import { sendMessage } from "@/services/message.service";
+import { getMessageList, sendMessage } from "@/services/message.service";
 import { toast } from "sonner";
 import { useSocketStore } from "@/store/useSocketStore";
 
-const ChatWindow = ({ chatId }: { chatId: string }) => {
+const ChatWindow = ({ chatId,userId }: { chatId: string ,userId: string}) => {
   const [message, setMessage] = React.useState("");
-  const [messageList,setMessageList] = React.useState([]);
-  
+  const [messageList, setMessageList] = React.useState([]);
+
+  useEffect(() => {
+    const fetchMessageList = async () => {
+      try {
+        const messageList = await getMessageList(chatId);
+        setMessageList(messageList);
+      } catch (error) {
+        console.error(error);
+        toast.error("Failed to fetch messages");
+      }
+    };
+    fetchMessageList();
+  }, [chatId]);
+
   const socket = useSocketStore((state) => state.socket);
 
   const handleSubmit = async (e: React.FormEvent) => {
@@ -28,7 +41,7 @@ const ChatWindow = ({ chatId }: { chatId: string }) => {
 
   return (
     <div className=" w-full min-h-screen relative p-6 justify-between flex flex-col">
-      <ChatMessage />
+      <ChatMessage messageList={messageList} userId={userId}/>
       <form onSubmit={handleSubmit} className="flex h-16">
         <Input
           onChange={(e) => setMessage(e.target.value)}
