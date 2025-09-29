2da47cb0293a3e4b4001f2f2182937e90cf0aad4
colombefioren <colomberakotonjanahary@gmail.com>
2025-09-29 15:24:48 +0300

    feat: implement send message logic in client

--- src/components/chat/chat-panel.tsx
+++ src/components/chat/chat-panel.tsx
@@ -18,7 +18,7 @@ const ChatPanel = ({ userId }: { userId: string }) => {
 
       <div className="flex-1">
         {selectedChat ? (
-          <ChatWindow/>
+          <ChatWindow chatId={selectedChat.id}/>
         ) : (
           <>
             <h1 className="text-2xl font-bold text-gray-900 dark:text-gray-100">
--- src/components/chat/chat-window.tsx
+++ src/components/chat/chat-window.tsx
@@ -2,19 +2,38 @@ import React from "react";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
 import ChatMessage from "./chat-message";
+import { sendMessage } from "@/services/message.service";
+import { toast } from "sonner";
 
-const ChatWindow = () => {
+const ChatWindow = ({ chatId }: { chatId: string }) => {
+  const [message, setMessage] = React.useState("");
 
-    const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault();
-        alert("Message sent")
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (message.trim() === "") return;
+
+    try {
+      const newMessage = await sendMessage({ chatId, content: message });
+      console.log(newMessage);
+    } catch (error) {
+      console.error(error);
+      toast.error("Failed to send message");
     }
+  };
+
   return (
     <div className=" w-full min-h-screen relative p-6 justify-between flex flex-col">
       <ChatMessage />
       <form onSubmit={handleSubmit} className="flex h-16">
-        <Input className="h-full" placeholder="Send a message..."/>
-        <Button type="submit" className="h-full w-26">Send</Button>
+        <Input
+          onChange={(e) => setMessage(e.target.value)}
+          className="h-full"
+          placeholder="Send a message..."
+        />
+        <Button type="submit" className="h-full w-26">
+          Send
+        </Button>
       </form>
     </div>
   );
