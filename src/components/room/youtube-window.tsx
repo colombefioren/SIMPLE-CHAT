1baadac26a0984c9d514864871c88c39b1dfdd4b
colombefioren <colomberakotonjanahary@gmail.com>
2025-10-01 09:55:33 +0300

    feat: utility function to extract video id from youtube link

--- src/components/room/youtube-window.tsx
+++ src/components/room/youtube-window.tsx
@@ -4,20 +4,25 @@ import { Button } from "../ui/button";
 import { Input } from "../ui/input";
 import { useEffect } from "react";
 import { Room } from "@/types/room";
+import { useUserStore } from "@/store/useUserStore";
 
 const YoutubeWindow = ({ room }: { room: Room }) => {
   const socket = useSocketStore((state) => state.socket);
-
+  const user = useUserStore((state) => state.user);
   useEffect(() => {
     socket?.emit("join-room", room.id);
   }, [socket, room.id]);
 
+  const isHost = room.createdBy === user?.id;
+
   return (
     <div className="flex flex-col justify-between">
-      <div className="flex">
-        <Input placeholder="Place the youtube link here" type="text" />
-        <Button>Load</Button>
-      </div>
+      {isHost && (
+        <div className="flex">
+          <Input placeholder="Place the youtube link here" type="text" />
+          <Button>Load</Button>
+        </div>
+      )}
     </div>
   );
 };
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,30 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function extractYtVideoId(url: string): string | null {
+  try {
+    const parsedUrl = new URL(url);
+
+    if (parsedUrl.hostname.includes("youtube.com")) {
+      const v = parsedUrl.searchParams.get("v");
+      if (v) return v;
+      const pathParts = parsedUrl.pathname.split("/");
+      if (pathParts.includes("embed")) {
+        return pathParts[pathParts.indexOf("embed") + 1] || null;
+      }
+    }
+
+    if (parsedUrl.hostname === "youtu.be") {
+      return parsedUrl.pathname.slice(1) || null;
+    }
+
+    return null;
+  } catch (e) {
+    console.error("Invalid URL:", url);
+    return null;
+  }
 }
